285dc09c9050c10deb3a8a95f53fb6f957e7abd7
Cebo <cebo@cebocampbell.com>
2015-06-07 07:57:19 -0500

    A round of homepage changes

--- wp-content/themes/ecp/index.php
+++ wp-content/themes/ecp/index.php
@@ -2,13 +2,18 @@
 
 
 	<div class="ecp-home">
+		
+		
+		<div class="ecp-home-lining">
+		
+			<img src="<?php bloginfo('template_url'); ?>/images/homepage-ecp-logo.png">
 
-		<img src="<?php bloginfo('template_url'); ?>/images/homepage-ecp-logo.png">
-
-		<ul class="menu">
-			<?php wp_nav_menu( array( 'theme_location' => 'primary' ,  'container' => false, 'items_wrap' => '%3$s' ) ); ?>
-		</ul>
-
+			<ul class="menu">
+				<?php wp_nav_menu( array( 'theme_location' => 'primary' ,  'container' => false, 'items_wrap' => '%3$s' ) ); ?>
+			</ul>
+			
+		</div>
+		
 	</div>
 	
 
