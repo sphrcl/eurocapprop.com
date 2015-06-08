680a8f5612559e914c655fac4b47f358722439c2
jeromemisfit <jerome@misfit-inc.com>
2015-06-09 00:41:35 +0800

    contact page

--- wp-content/themes/ecp/page-contact.php
+++ wp-content/themes/ecp/page-contact.php
@@ -27,7 +27,7 @@ get_header();
 
 		<div class="map-container">
 			<!-- <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3022.2992498223907!2d-73.98093779999999!3d40.7554425!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c258ffc5430ca7%3A0xd7b0da21781d6dca!2s19+W+44th+St+%231702%2C+New+York%2C+NY+10036%2C+USA!5e0!3m2!1sen!2s!4v1433131422540" width="800" height="600" frameborder="0" style="border:0"></iframe> -->
-			<img width="1200" height="auto" src="<?php bloginfo ('template_url'); ?>/images/eurocap-map-contact.jpg" />
+			<img src="<?php bloginfo ('template_url'); ?>/images/eurocap-map-contact.jpg" />
 		</div>
 	
 	</div>
