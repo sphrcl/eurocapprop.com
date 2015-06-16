ded3e84bb97cf68e64ca1854d46e84b8dad0d569
Jhelline <jhelline.fausto@outsourced.ph>
2015-06-17 03:46:04 +0800

    add open profile box to leadership page like it does on development page

--- wp-content/themes/ecp/page-partnership.php
+++ wp-content/themes/ecp/page-partnership.php
@@ -25,28 +25,31 @@ get_header();
 				if($query_partnership->have_posts()) {
 			?>
 
-				<ul class="team">
+				<ul class="team mem">
 
 					<?php
+						$count = 2;
 						while($query_partnership->have_posts()) : $query_partnership->the_post();
 						$imgsrc = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), "Full"); 
 					?>
+						<?php if($count > 1) { ?>
 					
-						<li>
-							
-								<a target="_blank" href="<?php echo get_post_meta($post->ID, 'misfit_link', true); ?>">
-									<div class="partner-thumb" style="background-image: url(<?php echo tt($imgsrc[0],320,240); ?>);">
+							<li class="memli">
+								
+									<!--<a target="_blank" href="<?php echo get_post_meta($post->ID, 'misfit_link', true); ?>">-->
+										<div class="partner-thumb" style="background-image: url(<?php echo tt($imgsrc[0],320,240); ?>);"><a class="dropanchor mem-detail mem<?php echo $count;?>"></a></div>
+									<!--</a>-->
+									<h3><?php the_title(); ?></h3>
+									<h4><?php echo get_post_meta($post->ID,'misfit_industry',true); ?></h4>
+									<div class="person-description">
+										<div class="left"><?php the_content(); ?></div>
 									</div>
-								</a>
-								<h3><?php the_title(); ?></h3>
-								<h4><?php echo get_post_meta($post->ID,'misfit_industry',true); ?></h4>
-								<div class="person-description">
-									<div class="left"><?php the_content(); ?></div>
-								</div>
-							
-						</li>
+								
+							</li>
 
-					<?php endwhile; ?>
+					<?php } ?>
+
+				<?php $count++; endwhile; ?>
 
 				</ul>
 
@@ -55,6 +58,48 @@ get_header();
 		</div>	
 
 	</div>
+
+		<?php
+		$query_partnership = new wp_query(array(
+					'post_type' => 'partnership',
+				));
+
+		if($query_partnership->have_posts()) {
+	?>
+			
+		<?php
+			$count = 2;
+			while($query_partnership->have_posts()) : $query_partnership->the_post();
+						$imgsrc = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), "Full"); 
+		?>
+			<?php if($count > 1) { ?>
+			
+				<div class="memberwrap mem<?php echo $count;?>" style="display: none;">
+					<div class="mw-container">
+					<div class="mw-open">
+						
+						<div class="mw-img" style="background-image: url(<?php echo tt($imgsrc[0],450,310); ?>);"></div>
+						
+						<div class="mw-content">
+							<h3><?php the_title(); ?></h3>
+							<h4><?php echo get_post_meta($post->ID,'misfit_industry',true); ?></h4>
+							<div class="mw-desc"><?php the_content(); ?></div>
+						</div>
+						
+						<div class="mem-close"></div>
+						
+						<div class="clear"></div>
+					
+					</div>
+					</div>
+				</div>
+				
+			<?php } ?>
+			
+			<?php $count++; endwhile; ?>
+		
+	<?php } wp_reset_query(); ?>
+
 
 </div>
 
