0a34327cbdcb88e7b2e4451edce297a95415bacc
Jhelline <jhelline.fausto@outsourced.ph>
2015-06-18 23:49:20 +0800

    add open profile box to leadership page like it does on development page

--- wp-content/themes/ecp/page-development.php
+++ wp-content/themes/ecp/page-development.php
@@ -41,14 +41,15 @@ get_header();
 				if($query_development_us->have_posts()) {
 			?>
 
-				<ul class="team">
+				<ul class="team mem">
 
 					<?php
+						$count = 2;
 						while($query_development_us->have_posts()) : $query_development_us->the_post();
 						$imgsrc = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), "Full"); 
 					?>
-					
-						<li>
+						<?php if($count > 1) { ?>
+							<li class="memli">
 							
 							<div class="right dev-slider owl-carousel owl-theme">
 
@@ -78,12 +79,15 @@ get_header();
 							<h3><?php the_title(); ?></h3>
 							<h4><?php echo get_post_meta($post->ID,'misfit_location',true); ?></h4>
 							<div class="person-description">
-								<div class="left"><?php the_content(); ?></div>
+								<div class="left"><?php echo excerpt(20); ?></div>
+								<a class="mem-detail mem<?php echo $count;?>" href="#"><img src="<?php bloginfo('template_url'); ?>/images/right-arrow.png"></a>
 							</div>
 
 						</li>
 
-					<?php endwhile; ?>
+					<?php } ?>
+
+				<?php $count++; endwhile; ?>
 
 				</ul>
 
@@ -91,7 +95,79 @@ get_header();
 
 		</div>	
 
-		<div class="development-divider">
+
+			<?php
+		$query_development_us = new wp_query(array(
+					'post_type' => 'development',
+					'tax_query' => array(
+						array(
+							'taxonomy' => 'development-type',
+							'field'    => 'slug',
+							'terms'    => 'united-states',
+						),
+					),
+				)); 
+
+				if($query_development_us->have_posts()) {
+	?>
+			
+		<?php
+			$count = 2;
+			while($query_development_us->have_posts()) : $query_development_us->the_post();
+						$imgsrc = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), "Full");
+		?>
+			<?php if($count > 1) { ?>
+			
+				<div class="memberwrap mem<?php echo $count;?>" style="display: none;">
+					<div class="mw-container">
+					<div class="mw-open">
+				
+						<div class="left dev-slider owl-carousel owl-theme">
+
+								<?php 
+									$galleryImages = get_post_gallery_imagess(); 
+									$imagesCount = count($galleryImages);
+								?>
+							           
+					    		<?php if ($imagesCount > 0) : ?>
+					          	<?php for ($i = 0; $i < $imagesCount; $i++): ?>
+					            <?php if (!empty($galleryImages[$i])) :?>
+
+									<div class="mw-img" style="background-image: url(<?php echo tt($galleryImages[$i]['full'][0],320,240); ?>);"></div>
+								
+								<?php endif; ?>
+								<?php endfor; ?>
+								
+								<?php else : ?>
+								
+									<div class="mw-img" style="background-image: url(<?php echo tt($imgsrc[0],450,310); ?>);"></div>
+									
+									
+								<?php endif; ?>
+
+							</div>
+						
+						<div class="mw-content">
+							<h3><?php the_title(); ?></h3>
+							<h4><?php echo get_post_meta($post->ID,'misfit_title_position',true); ?></h4>
+							<div class="mw-desc"><?php the_content(); ?></div>
+						</div>
+						
+						<div class="mem-close"></div>
+						
+						<div class="clear"></div>
+					
+					</div>
+					</div>
+				</div>
+				
+			<?php } ?>
+			
+			<?php $count++; endwhile; ?>
+		
+	<?php } wp_reset_query(); ?>
+
+			<div class="development-divider">
 
 			<h2>Properties | International</h2>
 
@@ -110,14 +186,16 @@ get_header();
 				if($query_development_international->have_posts()) {
 			?>
 
-				<ul class="team">
+				<ul class="team mem1">
 
 					<?php
+						$count = 2;
 						while($query_development_international->have_posts()) : $query_development_international->the_post();
 						$imgsrc = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), "Full"); 
 					?>
 					
-						<li>
+						<?php if($count > 1) { ?>
+							<li class="memli">
 							
 							<div class="right dev-slider owl-carousel owl-theme">
 
@@ -141,20 +219,86 @@ get_header();
 							<h3><?php the_title(); ?></h3>
 							<h4><?php echo get_post_meta($post->ID,'misfit_location',true); ?></h4>
 							<div class="person-description">
-								<div class="left"><?php the_content(); ?></div>
+								<div class="left"><?php echo excerpt(20); ?></div>
+								<a class="mem-detail mem1<?php echo $count;?>" href="#"><img src="<?php bloginfo('template_url'); ?>/images/right-arrow.png"></a>
 							</div>
 
 						</li>
 
-					<?php endwhile; ?>
+					<?php } ?>
+
+				<?php $count++; endwhile; ?>
 
 				</ul>
 
 			<?php } wp_reset_query(); ?>
 		
-		</div>	
+		</div>			<?php
+		$query_development_international = new wp_query(array(
+					'post_type' => 'development',
+					'tax_query' => array(
+						array(
+							'taxonomy' => 'development-type',
+							'field'    => 'slug',
+							'terms'    => 'international',
+						),
+					),
+				)); 
 
-	</div>
+				if($query_development_international->have_posts()) {
+	?>
+			
+		<?php
+			$count = 2;
+			while($query_development_international->have_posts()) : $query_development_international->the_post();
+			$imgsrc = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), "Full");
+		?>
+			<?php if($count > 1) { ?>
+			
+				<div class="memberwrap mem1<?php echo $count;?>" style="display: none;">
+					<div class="mw-container">
+					<div class="mw-open">
+				
+						<div class="left dev-slider owl-carousel owl-theme">
+
+								<?php 
+									$galleryImages = get_post_gallery_imagess(); 
+									$imagesCount = count($galleryImages);
+								?>
+							           
+					    		<?php if ($imagesCount > 0) : ?>
+					          	<?php for ($i = 0; $i < $imagesCount; $i++): ?>
+					            <?php if (!empty($galleryImages[$i])) :?>
+
+									<div class="mw-img" style="background-image: url(<?php echo tt($galleryImages[$i]['full'][0],320,240); ?>);"></div>
+								
+								<?php endif; ?>
+								<?php endfor; ?>
+								<?php endif; ?>
+
+							</div>
+						
+						<div class="mw-content">
+							<h3><?php the_title(); ?></h3>
+							<h4><?php echo get_post_meta($post->ID,'misfit_title_position',true); ?></h4>
+							<div class="mw-desc"><?php the_content(); ?></div>
+						</div>
+						
+						<div class="mem-close"></div>
+						
+						<div class="clear"></div>
+					
+					</div>
+					</div>
+				</div>
+				
+			<?php } ?>
+			
+			<?php $count++; endwhile; ?>
+		
+	<?php } wp_reset_query(); ?>
+
+
 
 </div>
 
